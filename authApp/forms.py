fc589fbce6c696b019b556ee4a54697fe949ed2d
Sriram Thangavel <sriramthangavel.developer@gmail.com>
2023-10-25 10:51:39 +0530

    styled-login-form

--- authApp/forms.py
+++ authApp/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 from django.contrib.auth.models import User
 
-
 class LoginForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['username','password']
+        fields = ['username', 'password']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
+            'password': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
+        }
--- authApp/views.py
+++ authApp/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth import login,logout,authenticate
+from  .forms import LoginForm
+from django.http import HttpResponse
 
 def register(request):
     return render(request,'authApp/register.html')
 
 def login(request):
-    return render(request,'authApp/login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request,username=username,password=password)
+        if user is None:
+            return HttpResponse('invalid credentials')
+        
+        login(request,user)
+        return HttpResponse('success')
+    else:
+        form = LoginForm()
+        return render(request,'authApp/login.html',{'form':form})
 
 def logout(request):
     return render(request,'authApp/register.html')
